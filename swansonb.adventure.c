d97fe713ba06d14d474c182c43df85dff06f5816
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-04-29 18:50:54 -0700

    first commit, names of rooms established

---
+++ swansonb.adventure.c
@@ -0,0 +1,10 @@
+/*
+ * swansonb.adventure.c
+ *
+ *  Created on: Apr 29, 2015
+ *      Author: Brandon Swanson
+ */
+
+#include <string.h>
+
+const char * const ROOM_NAMES[] = { "Fred's Room","Music Room", "Darkroom", "Observatory", "Attic", "Dungeon", "Kitchen", "Tentacle's Room", "Ed's Room", "Front Porch"};
